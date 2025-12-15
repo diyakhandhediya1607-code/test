d01400f53d0c1751d15a06b8449b6986aa2fbd78
diyakhandhediya1607-code <diyakhandhediya1607@gmail.com>
2025-12-15 23:14:39 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,31 @@
+int main(){
+    float c;
+    printf("Enter value of celsius");
+    scanf("%f" , &c);
+    float F = (9.0/5.0 * c) + 32;
+    printf("the temparature is fahrenheit = %f ", F);
+
+    int bs;
+
+     printf("\n Enter the value of salary");
+    scanf("%d" , &bs);
+
+    int A,B,C ;
+    printf("Enter the value of A");
+    scanf("%d" , &A);
+
+    printf("\n Enter the value of B");
+    scanf("%d" , &B);
+
+    printf("\nEnter the value of C");
+    scanf("%d" , &C);
+
+
+
+      
+ 
+
+
+    
+}
+
